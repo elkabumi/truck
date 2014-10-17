67771c690f8d46fe0ece682a0fb9616f0b29ccb5
candramelon <selubungers@gmail.com>
2014-10-17 11:08:57 +0700

    17/10/2014

--- controllers/login.php
+++ controllers/login.php
@@ -41,14 +41,13 @@ switch ($page) {
 			$_SESSION['user_id'] = $query_user->user_id;
 			$_SESSION['user_type_id'] = $query_user->user_type_id;
 			
-			/*
+			
 			if($_SESSION['user_type_id'] == 1 || $_SESSION['user_type_id'] == 3){
 				header("Location: transaction.php?page=list");
 			}else{
 				header('Location: report_detail.php?page=list');
 			}
-			*/
-			header('Location: master.php?page=list');
+			
 			
 			echo $_SESSION['user_type_id'];
 			
